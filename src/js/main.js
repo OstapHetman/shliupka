1e129cc9f510cc567e10143eabccdaf510be7257
OstapHetman <prickly4528@gmail.com>
2018-03-14 09:23:39 +0200

    implement some responsive

--- src/js/main.js
+++ src/js/main.js
@@ -34,17 +34,15 @@ $(document).ready(function() {
     });
 });
 // Dynamical text
-window.onload = function() {
-    var title = document.getElementById('titleText'),
-        btn = document.getElementById('buttonSubmit'),
-        getBtns = document.getElementsByClassName('paste-btn'),
-        lengthBtns = getBtns.length;
-    for (let i = 0; i < lengthBtns; i++) {
-        getBtns[i].onclick = function() {
-            let titleText = this.getAttribute('data-title');
-            let btnText = this.getAttribute('data-btn');
-            title.innerText = titleText;
-            btn.innerText = btnText;
-        }
+var title = document.getElementById('titleText'),
+    btn = document.getElementById('buttonSubmit'),
+    getBtns = document.getElementsByClassName('paste-btn'),
+    lengthBtns = getBtns.length;
+for (let i = 0; i < lengthBtns; i++) {
+    getBtns[i].onclick = function() {
+        let titleText = this.getAttribute('data-title');
+        let btnText = this.getAttribute('data-btn');
+        title.innerText = titleText;
+        btn.innerText = btnText;
     }
 }
